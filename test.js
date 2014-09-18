4705a012cee8bcbac3566e6feb8c93c405ee878e
Darel Finkbeiner <darel.finkbeiner@gmail.com>
2014-09-18 14:04:45 -0400

    verify nested json

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ var dec = prototrans.Decoder(schema)
 var test = require('tape')
 
 test('encoder/decoder test', function(t) {
-  t.plan(1)
+  t.plan(2)
   enc.pipe(dec)
 
   var testmsg = {msg: 'Another Message', id: 16}
@@ -22,5 +22,17 @@ test('encoder/decoder test', function(t) {
     
   })
   enc.write(JSON.stringify(testmsg))
+
+  var moreComplexMsg = {msg: '[{"object1": "value1"},{"object2": "value2"}]', id: 12}
+  var dec2 = prototrans.Decoder(schema)
+  dec2.on('data', function(data) {
+    var d = JSON.parse(data)
+    var embeddedMsg = JSON.parse(d.msg)
+    var shouldbe = JSON.parse(moreComplexMsg.msg)
+    t.deepEqual(embeddedMsg, shouldbe, 'nested message')
+  })
+  var enc2 = prototrans.Encoder(schema)
+  enc2.pipe(dec2)
+  enc2.write(JSON.stringify(moreComplexMsg))
   
 })
